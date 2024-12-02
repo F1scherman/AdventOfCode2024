e7e659ea240fd9b330ca6014804701002fe2dc43
BraydenJonsson <brayden.f.jonsson@gmail.com>
2024-12-01 20:05:15 -0700

    day one part one done

--- day_one/src/main.rs
+++ day_one/src/main.rs
@@ -1,3 +1,23 @@
 fn main() {
-    println!("Hello, world!");
+    let input = include_str!("example.txt");
+    part_one(input);
+}
+
+fn part_one(input: &str) {
+    let mut left = vec![];
+    let mut right = vec![];
+    input.lines().for_each(|line| {
+        let mut it = line.split("   ");
+        left.push(it.next().unwrap().parse::<i32>().unwrap());
+        right.push(it.next().unwrap().parse::<i32>().unwrap());
+    });
+    left.sort();
+    right.sort();
+
+    let mut sum = 0;
+    for i in 0..left.len() {
+        sum += (left[i] - right[i]).abs();
+    }
+
+    println!("{}", sum);
 }
