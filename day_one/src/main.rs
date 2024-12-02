5dcd70937a7c47e82271e57ed9a6edd2ae3c447b
BraydenJonsson <brayden.f.jonsson@gmail.com>
2024-12-01 20:17:14 -0700

    part 2 done

--- day_one/src/main.rs
+++ day_one/src/main.rs
@@ -1,6 +1,8 @@
+use std::collections::{HashMap, HashSet};
+
 fn main() {
-    let input = include_str!("example.txt");
-    part_one(input);
+    let input = include_str!("input.txt");
+    part_two(input);
 }
 
 fn part_one(input: &str) {
@@ -17,6 +19,28 @@ fn part_one(input: &str) {
     let mut sum = 0;
     for i in 0..left.len() {
         sum += (left[i] - right[i]).abs();
+    }
+
+    println!("{}", sum);
+}
+
+fn part_two(input: &str) {
+    let mut left = vec![];
+    let mut right = HashMap::new();
+    input.lines().for_each(|line| {
+        let mut it = line.split("   ");
+        left.push(it.next().unwrap().parse::<i32>().unwrap());
+        let right_key = it.next().unwrap().parse::<i32>().unwrap();
+        if right.contains_key(&right_key) {
+            right.insert(right_key, right[&right_key] + 1);
+        } else {
+            right.insert(right_key, 1);
+        }
+    });
+
+    let mut sum = 0;
+    for i in left.iter() {
+        sum += right.get(i).unwrap_or(&0) * i;
     }
 
     println!("{}", sum);
